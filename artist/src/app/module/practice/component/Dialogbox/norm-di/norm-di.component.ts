37a72bc0150bf1331025f2ab1b41f0b867bb5a48
krishnna <krishnayera@gmail.com>
2024-02-25 19:54:53 +0530

    new changes about formsArray

--- artist/src/app/module/practice/component/Dialogbox/norm-di/norm-di.component.ts
+++ artist/src/app/module/practice/component/Dialogbox/norm-di/norm-di.component.ts
@@ -10,43 +10,45 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 
 export class NormDiComponent {
-  allDegrees:any = [{id:1, name:'HSC'},{id:2, name:'Degree'}, {id:3, name:'Master Degree'},{id:4, name:'phd'}];
+
+  allDegrees:any = [{id:1, name:'SSC'}, {id:2, name:'HSC'}, {id:3, name:'BSC'}, {id:4, name:'MSC'}];
   constructor(@Inject(MAT_DIALOG_DATA) public data:any, public _fb:FormBuilder){}
 
   normForm = this._fb.group({
     firstName:      ['',[Validators.required]],
     lastName:       ['',[Validators.required]],
-    emailId:        ['',[Validators.required,Validators.email]],
+    emailId:        ['',[Validators.required, Validators.email]],
     dofBirth:       ['',[Validators.required]],
-    phone:          ['',[Validators.required,Validators.pattern(/^\+?\d{10,15}$/), Validators.maxLength(10)]],
+    phone:          ['',[Validators.required, Validators.minLength(10),Validators.maxLength(10)]],
     gender:         ['',[Validators.required]],
     education:      ['',[Validators.required]],
     company:        ['',[Validators.required]],
     experience:     ['',[Validators.required]],
     package:        ['',[Validators.required]],
-    degrees:         this._fb.array([])
-  });
+    degrees:        this._fb.array([])
+  })
 
-  ngOnInit(){
+  addItems(){
+    const item = this.normForm.get('degrees') as FormArray;
+    item.push(this.createItems());
   }
 
-  addDegree(){
-    const degree = this.normForm.get('degrees') as FormArray;
-    degree.push(this.createDegree());
-  }
-
-  createDegree(){
-    return this._fb.group({
+  createItems(){
+   return this._fb.group({
       type:   ['',[Validators.required]],
       file:   ['',[Validators.required]]
-    })
+   })
   }
 
-  get getDegree(){
-    return this.normForm.get('degrees') as FormArray;
+  get getAllItems(){
+    return this.normForm.get('degrees') as FormArray; 
   }
 
-  onFormSubmit(){
-    console.log(this.normForm);
+  insertFile(event:any){
+
+  }
+
+  ngSubmitRecord(){
+    console.log(this.normForm.value);
   }
 }
